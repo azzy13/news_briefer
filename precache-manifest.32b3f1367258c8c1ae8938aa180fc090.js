924458331f107aa72a18d9f620d32e2616b0da56
azzy13 <azharhasan013@gmail.com>
2020-08-29 23:20:38 +0530

    Updates

---
+++ precache-manifest.32b3f1367258c8c1ae8938aa180fc090.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f74081d304b9c48b03cb869718373e",
+    "url": "/news_briefer/index.html"
+  },
+  {
+    "revision": "791402b21bbf2c98bb76",
+    "url": "/news_briefer/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "08da5721b04f3e71f7d0",
+    "url": "/news_briefer/static/css/main.dcc17fe0.chunk.css"
+  },
+  {
+    "revision": "791402b21bbf2c98bb76",
+    "url": "/news_briefer/static/js/2.7bb09034.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/news_briefer/static/js/2.7bb09034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08da5721b04f3e71f7d0",
+    "url": "/news_briefer/static/js/main.d7c8a5b8.chunk.js"
+  },
+  {
+    "revision": "554417cac519dd94c7b3",
+    "url": "/news_briefer/static/js/runtime-main.e50aa7c4.js"
+  },
+  {
+    "revision": "45eed2f1bfaf3bfa2b08c96b5934ba06",
+    "url": "/news_briefer/static/media/bgimg.45eed2f1.jpg"
+  },
+  {
+    "revision": "fbace24914d7a52ffd6fe63238691be0",
+    "url": "/news_briefer/static/media/shk.fbace249.jpg"
+  }
+]);
