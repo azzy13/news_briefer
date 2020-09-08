9fcc348607810c6d8568bc116ec09403c6e0bb62
azzy13 <azharhasan013@gmail.com>
2020-09-08 18:13:02 +0530

    Btns

--- src/components/pages/NewsItem.js
+++ src/components/pages/NewsItem.js
@@ -29,15 +29,44 @@ const NewsItem = ({ NewsReducer }) => {
         <strong>Source: {NewsReducer.source.name}</strong>
       </p>
       <br />
-      <div className='center-align cus_flex'>
+      <div className='center-align'>
         <a
           target='_blank'
           rel='noopener noreferrer'
           href={NewsReducer.url}
-          className='btn btn-pink btn-sm my-1 cus_btn'
+          className='btn btn-pink btn-sm my-1 cus_btn_left'
         >
           Full Story Here &#10132;
         </a>
+      </div>
+      <div className='right'>
+        <ul>
+          <li>
+            <a
+              href={
+                'https://www.facebook.com/sharer/sharer.php?u=' +
+                NewsReducer.url
+              }
+              className='waves-effect waves-light btn social facebook cus_btn_right'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fa fa-facebook'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href={
+                'https://www.twitter.com/intent/tweet?text=' + NewsReducer.url
+              }
+              className='waves-effect waves-light btn social twitter cus_btn_middle'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fa fa-twitter'></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
